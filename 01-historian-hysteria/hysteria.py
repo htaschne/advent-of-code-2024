56b3a55ce509f65e4fa3f423503ee234d4b69ace
Agatha Schneider <agatha.schneider@outlook.com>
2024-12-01 20:48:25 -0800

    yesterday

---
+++ 01-historian-hysteria/hysteria.py
@@ -0,0 +1,20 @@
+
+import sys
+
+from collections import Counter
+
+ranges = [list(map(int, line.strip().split("   "))) for line in open(sys.argv[1]).readlines()]
+
+lo = [r[0] for r in ranges]
+hi = [r[1] for r in ranges]
+
+lo.sort()
+hi.sort()
+
+dists = [abs(l - h) for l, h in zip(lo, hi)]
+acc = sum(dists)
+print(acc)
+
+times = Counter(lo)
+acc = sum([x * times[x] for x in hi])
+print(acc)
