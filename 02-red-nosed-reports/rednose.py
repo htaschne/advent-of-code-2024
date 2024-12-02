90552d5038378f490d67c493d3f7c1c35169b2b1
Agatha Schneider <agatha.schneider@outlook.com>
2024-12-01 21:39:53 -0800

    day 2!

---
+++ 02-red-nosed-reports/rednose.py
@@ -0,0 +1,32 @@
+
+import sys
+
+
+def check(level):
+    incs = [(p - n) for p, n in zip(level, level[1:])]
+    pos = all([x > 0 for x in incs])
+    neg = all([x < 0 for x in incs])
+    par = all([abs(x) <= 3 and abs(x) >= 1 for x in incs])
+    return (pos or neg) and par
+
+def main():
+  levels = [list(map(int, l.strip().split(" ")))for l in open(sys.argv[1]).readlines()]
+  acc = sum([check(level) for level in levels])
+  print(acc)
+
+  acc = 0
+  for level in levels:
+    if check(level):
+      acc += 1
+    else:
+      for i in range(len(level)):
+        nl = level[:i] + level[i+1:]
+        if check(nl):
+          acc += 1
+          break
+  print(acc)
+
+
+
+if __name__ == "__main__":
+  main()
