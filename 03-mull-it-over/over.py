0306f57ea69ef181ddf2c3169a96d545b70cc78e
Agatha Schneider <agatha.schneider@outlook.com>
2024-12-03 07:21:01 -0800

    refactor

--- 03-mull-it-over/over.py
+++ 03-mull-it-over/over.py
@@ -4,16 +4,16 @@ import sys
 
 
 def main():
-    pat = r"mul\((\d+),(\d+)\)"
-    acc = sum(
-        [
-            sum([int(m[0]) * int(m[1]) for m in re.findall(pat, line.strip())])
-            for line in open(sys.argv[1]).readlines()
-        ]
-    )
+  pat = r"mul\((\d+),(\d+)\)"
+  acc = sum(
+    [
+      sum([int(m[0]) * int(m[1]) for m in re.findall(pat, line.strip())])
+      for line in open(sys.argv[1]).readlines()
+    ]
+  )
 
-    print(acc)
+  print(acc)
 
 
 if __name__ == "__main__":
-    main()
+  main()
