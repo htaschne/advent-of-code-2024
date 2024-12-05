fa385da607a279a0c71d0e4dcbdaaf88867f899d
Agatha Schneider <agatha.schneider@outlook.com>
2024-12-05 09:13:33 -0800

    cleanup day 4 part 1

--- 04-ceres-search/search.py
+++ 04-ceres-search/search.py
@@ -6,38 +6,33 @@ def rows_to_cols(grid: list[str]) -> list[str]:
   return ["".join([grid[j][i] for j in range(len(grid))]) for i in range(len(grid))]
 
 def walk_down(pos: tuple[int, int], grid: list[str]) -> list[str]:
-  row = []
-  for i in range(len(grid)):
-    if 0 <= i + pos[0] < len(grid) and 0 <= i + pos[1] < len(grid):
-      row.append(grid[i + pos[0]][i + pos[1]])
-  return "".join(row)
+  return "".join([
+    grid[i + pos[0]][i + pos[1]]
+    for i in range(len(grid))
+    if 0 <= i + pos[0] < len(grid) and 0 <= i + pos[1] < len(grid)
+  ])
 
 
 def right_diagonals(grid: list[str]) -> list[str]:
-  rows = [walk_down((i, 0), grid) for i in range(1, len(grid))]
-  cols = [walk_down((0, i), grid) for i in range(len(grid))]
-  return rows + cols
-
+  return (
+    [walk_down((i, 0), grid) for i in range(len(grid))]
+  + [walk_down((0, i), grid) for i in range(1, len(grid))]
+  )
+  
 
 def walk_up(pos: tuple[int, int], grid: list[str]) -> list[str]:
-  row = []
-  for i in range(len(grid)):
-    if 0 <= pos[0] - i < len(grid) and 0 <= pos[1] + i < len(grid):
-      row.append(grid[pos[0] - i][pos[1] + i])
-  ret = "".join(row)
-  return ret
+  return "".join([
+    grid[pos[0] - i][pos[1] + i]
+    for i in range(len(grid))
+    if 0 <= pos[0] - i < len(grid) and 0 <= pos[1] + i < len(grid)
+  ])
 
 
 def left_diagonals(grid: list[str]) -> list[str]:
-  rows, cols = [], []
-  rows = [walk_up((len(grid) - 1 -i, 0), grid) for i in range(1, len(grid))]
-  cols = [walk_up((len(grid) - 1, i), grid) for i in range(len(grid))]
-  ret = rows + cols
-  return ret
-
-
-
-
+  return (
+    [walk_up((len(grid) - 1 -i, 0), grid) for i in range(len(grid))]
+  + [walk_up((len(grid) - 1, i), grid) for i in range(1, len(grid))]
+  )
 
 
 def search(grid: list[str]) -> tuple[int, int, int]:
@@ -45,12 +40,12 @@ def search(grid: list[str]) -> tuple[int, int, int]:
   v = sum([r.count("XMAS") + r.count("SAMX") for r in rows_to_cols(grid)])
   dr = sum([r.count("XMAS") + r.count("SAMX") for r in right_diagonals(grid)])
   dl = sum([r.count("XMAS") + r.count("SAMX") for r in left_diagonals(grid)])
-  return h, v, dr + dl
+  return sum([h, v, dr + dl])
 
 def main():
   g = [line.strip() for line in open(sys.argv[1]).readlines()]
-  h, v, d = search(g)
-  print(h + v + d)
+  xmas = search(g)
+  print(xmas)
 
 
 
