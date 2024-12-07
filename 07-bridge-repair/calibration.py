be29262d03fd083b8dda5f155a780fb39c3f39d5
Agatha Schneider <agatha.schneider@outlook.com>
2024-12-07 11:48:13 -0300

    day 7 part 2

--- 07-bridge-repair/calibration.py
+++ 07-bridge-repair/calibration.py
@@ -2,30 +2,33 @@
 import sys
 
 def check(result: int, operands: list[int], acc: int, idx: int = 1) -> bool:
-    if idx == len(operands) - 1:
-        return result == acc + operands[idx] or result == acc * operands[idx]
+  if idx == len(operands) - 1:
+    return (
+      result == acc + operands[idx]
+      or result == acc * operands[idx]
+      or result == int(str(acc) + str(operands[idx]))
+    )
 
-    curr = operands[idx]
-    if acc == 0:
-        return check(result, operands, idx + 1, curr)
+  curr = operands[idx]
+  if acc == 0:
+    return check(result, operands, idx + 1, curr)
 
-    plus = check(result, operands, acc + curr, idx + 1)
-    mult = check(result, operands, acc * curr, idx + 1)
-    return plus or mult
+  plus = check(result, operands, acc + curr, idx + 1)
+  mult = check(result, operands, acc * curr, idx + 1)
+  conc = check(result, operands, int(str(acc) + str(curr)), idx + 1)
+  return plus or mult or conc
 
 
 def main():
-
-    acc = 0
-    for line in open(sys.argv[1]).readlines():
-        x = line.strip().split(": ")
-        result = int(x[0])
-        operands = list(map(int, x[1].split(" ")))
-        if check(result, operands, operands[0]):
-            acc += result
-    print(acc)
-
+  acc = 0
+  for line in open(sys.argv[1]).readlines():
+    x = line.strip().split(": ")
+    result = int(x[0])
+    operands = list(map(int, x[1].split(" ")))
+    if check(result, operands, operands[0]):
+      acc += result
+  print(acc)
 
 
 if __name__ == "__main__":
-    main()
+  main()
