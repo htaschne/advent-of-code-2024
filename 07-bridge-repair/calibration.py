2d8a6c684a0c949cf1febe2a58dd2fed8b90cdf8
Agatha Schneider <agatha.schneider@outlook.com>
2024-12-07 11:42:43 -0300

    day 7 part 1

---
+++ 07-bridge-repair/calibration.py
@@ -0,0 +1,31 @@
+
+import sys
+
+def check(result: int, operands: list[int], acc: int, idx: int = 1) -> bool:
+    if idx == len(operands) - 1:
+        return result == acc + operands[idx] or result == acc * operands[idx]
+
+    curr = operands[idx]
+    if acc == 0:
+        return check(result, operands, idx + 1, curr)
+
+    plus = check(result, operands, acc + curr, idx + 1)
+    mult = check(result, operands, acc * curr, idx + 1)
+    return plus or mult
+
+
+def main():
+
+    acc = 0
+    for line in open(sys.argv[1]).readlines():
+        x = line.strip().split(": ")
+        result = int(x[0])
+        operands = list(map(int, x[1].split(" ")))
+        if check(result, operands, operands[0]):
+            acc += result
+    print(acc)
+
+
+
+if __name__ == "__main__":
+    main()
